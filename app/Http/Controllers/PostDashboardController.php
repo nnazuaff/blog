b609ad55e55c5da7a7b394e50519a6b188c98ccf
Fauzan <fauzanzha09@gmail.com>
2026-04-25 18:27:27 +0700

    Membuat folder khusus dashboard beserta komponen dashboard. Implementasi  halaman showing post di dashboard

--- app/Http/Controllers/PostDashboardController.php
+++ app/Http/Controllers/PostDashboardController.php
@@ -20,7 +20,7 @@ class PostDashboardController extends Controller
 
         }
 
-        return view('dashboard', ['posts' => $posts->paginate(5)->withQueryString()]);
+        return view('dashboard.index', ['posts' => $posts->paginate(5)->withQueryString()]);
 
     }
 
@@ -43,9 +43,9 @@ class PostDashboardController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        //
+        return view('dashboard.show', ['post' => $post]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,9 @@ Route::get('/contact', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('dashboard', [PostDashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [PostDashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard/{post:slug}', [PostDashboardController::class, 'show'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
